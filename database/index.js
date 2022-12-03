eb42d644ce5c166ffffc08f2ae8702577785f77b
Jason <j.vanschaik@tcrmbo.nl>
2022-12-03 12:59:08 +0100

    insert form

--- database/index.js
+++ database/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
 const SpeurtochtModel = require('./models/Speurtocht');
 
 app.use(express.json());
+app.use(cors())
 
 mongoose.connect(
   'mongodb+srv://jsonschaik:psw1234@crud.lt8zbt4.mongodb.net/speurtocht?retryWrites=true&w=majority',
@@ -13,12 +15,18 @@ mongoose.connect(
   }
 );
 
-app.get('/', async (req, res) => {
+app.post('/insert', async (req, res) => {
+
+  const speurtochtId = req.body.speurtochtId;
+  const vragenForm = req.body.vragenForm;
+  const antwoorden = req.body.antwoorden;
+  const correcteAntwoord = req.body.correcteAntwoord;
+
   const speurtocht = new SpeurtochtModel({
-    speurtochtId: 1,
-    vragenForm: 'Ik ben de beste',
-    antwoorden: 'ja',
-    correcteAntwoord: 'ja',
+    speurtochtId: speurtochtId,
+    vragenForm: vragenForm,
+    antwoorden: antwoorden,
+    correcteAntwoord: correcteAntwoord,
   });
 
   try {
--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
+import Axios from 'axios';
+import { useState } from 'react';
+import './App.css';
+
 function App() {
+  const [id, setId] = useState(0);
+  const [vraag, setVraag] = useState('');
+  const [antwoord, setAntwoord] = useState('');
+  const [correct, setCorrect] = useState('');
+
+  const addQuestions = (e) => {
+    e.preventDefault();
+    Axios.post('http://localhost:3001/insert', { 
+     
+      speurtochtId: id,
+      vragenForm: vraag,
+      antwoorden: antwoord,
+      correcteAntwoord: correct
+    })
+  };
+
   return (
     <div className="App">
-     
+      <h1>Speurtocht</h1>
+
+      <form action="">
+        <label>Id</label>
+        <input
+          type="number"
+          id="speurtochtId"
+          onChange={(e) => {
+            setId(e.target.value);
+          }}
+        />
+        <label>Vraag</label>
+        <input
+          type="text"
+          id="vragenForm"
+          onChange={(e) => {
+            setVraag(e.target.value);
+          }}
+        />
+        <label>Antwoord</label>
+        <input
+          type="text"
+          id="antwoorden"
+          onChange={(e) => {
+            setAntwoord(e.target.value);
+          }}
+        />
+        <label>Correct antwoord</label>
+        <input
+          type="text"
+          id="correcteAntwoord"
+          onChange={(e) => {
+            setCorrect(e.target.value);
+          }}
+        />
+        <button onClick={addQuestions}>Submit</button>
+      </form>
     </div>
   );
 }
